4bcfe4f20b67d0fb2826220d28d18738ff21f60a
josephredmond5 <josephredmond5@icloud.com>
2023-03-29 21:14:24 +0100

    forecast tests with added notes

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,5 +1,71 @@
 import React from "react";
 
 describe("ForecastSummary", () => {
-  it("renders", () => {});
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders correctly", () => {});
 });
+
+// In the first code block, the "import React from 'react'" line imports the React library, which is a JavaScript library used for building user interfaces. The code then sets up a test for a component called "ForecastSummary" using the "describe" function provided by a testing framework such as Jest or Mocha. Inside the "describe" function, the code defines an object called "validProps" which contains some example data that will be passed to the "ForecastSummary" component during testing. Finally, the code defines an empty test case using the "it" function, which will be filled out in the subsequent code blocks.
+
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
+describe("ForecastSummary", () => {
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+  });
+});
+
+// In the second code block, the "render" function from the "@testing-library/react" library is used to render the "ForecastSummary" component with the "validProps" defined in the previous code block. The "asFragment" method is then used to create a snapshot of the rendered component. This snapshot is essentially a serialized version of the rendered component, which can be compared to other snapshots in future tests to ensure that the component continues to render correctly. The snapshot is not actually tested in this code block, but is instead passed on to the next code block.
+
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
+describe("ForecastSummary", () => {
+  const validProps = {...};
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+
+// In the third code block, the "expect" function is used to make an assertion that the snapshot created in the previous code block matches a previously saved snapshot. The "toMatchSnapshot" method is used to compare the two snapshots. If the snapshots match, the test passes. If they do not match, the test fails. This code block completes the "it" test case defined in the first code block.
+

4f7fa4c4a5eb52c12b1cf54a52dd538a5d8a3d04
josephredmond5 <josephredmond5@icloud.com>
2023-04-03 13:36:51 +0100

    test added

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,31 @@
-import { render, screen } from "@testing-library/react";
-import App from "../components/App";
-import forecast from '../data/forecast.json';
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummaries from "../components/ForecastSummaries";
 
-describe("App", () => {
-  test("renders App component correctly", () => {
-     render(<App location={forecast.location} />);
-     const h1Element = screen.getByText(/Manchester, UK/i);
-     expect(h1Element).toBeInTheDocument();
+describe("ForecastSummaries", () => {
+  const validProps = [
+    {
+      date: 1111111,
+      description: "Stub description 1",
+      icon: "stubIcon1",
+      temperature: {
+        max: 22,
+        min: 12,
+      },
+    },
+    {
+      date: 2222222,
+      description: "Stub description2",
+      icon: "stubIcon2",
+      temperature: {
+        max: 24,
+        min: 13,
+      },
+    },
+  ];
+
+  it("renders correctly", () => {
+    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
-

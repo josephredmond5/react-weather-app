27d925b0e53ea455a3cedeb281c3974de50e6aa2
josephredmond5 <josephredmond5@icloud.com>
2023-04-04 21:42:49 +0100

    update

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,15 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import iconData from "../data/iconData.json";
 
-function ForecastSummary({ date, temperature, description, icon }) {
+function ForecastSummary({ date, temperature, description, weatherCode }) {
   const degreeSymbol = "\u00B0";
+
+  const getIconUrl = (code) => {
+    const prefix = code.toString().slice(0, 2);
+    const icon = iconData[prefix][0].icon;
+    return `https://openweathermap.org/img/wn/${icon}.png`;
+  };
 
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
 
       <div className="forecast-summary__icon" data-testid="forecast-icon">
-        <img src={icon} alt={description} />
+        <img src={getIconUrl(weatherCode)} alt={description} />
       </div>
       <div className="forecast-summary__temperature">
         {temperature && temperature.max && `${temperature.max}${degreeSymbol}C`}
@@ -22,7 +29,7 @@ function ForecastSummary({ date, temperature, description, icon }) {
 ForecastSummary.propTypes = {
   date: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
+  weatherCode: PropTypes.number.isRequired,
   temperature: PropTypes.shape({
     max: PropTypes.number.isRequired,
     min: PropTypes.number.isRequired,
